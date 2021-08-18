0e98294339a2191cae3d6208127c3a8245652a60
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-18 11:27:25 -0500

    setting up initial file structure

---
+++ README.md
@@ -0,0 +1,45 @@
+# the-colony
+uses React and Mongo to view Mars info, develop a profile, and join The Colony
+
+## Table of Contents
+
+  - [Description](#description)
+  - [Technologies](#technologies)
+  - [Deployment](#deployment)
+  - [License](#license)
+  - [Contact](#contact)
+  - [Screenshots](#screenshots)
+
+  ## Description
+
+  The Colony is a PC and mobile app designed to function as a placeholder.
+  
+  ## Technologies:
+
+  * Front-end: React/Jsx, HTML, CSS, Js
+  * Back-end: mongoose, MongoDB, Robo 3T, express
+  * Dev-deps: nodemon, Chrome's dev tools
+
+  ## Deployment:
+
+  Check out the app on Heroku:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)]()
+
+  ## License
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Contact
+
+  * If you have any questions/concerns regarding the app, please reach out to our GitHub profiles: brettfleming, Chaoly123, CortlandSB, goreno41, tedheikkila
+
+## Screenshots
+
+* home
+
+    ![](./client/public/screenshots/proj3-1.png)
+
+* log in/sign up
+
+    ![](./client/public/screenshots/proj3-2.png)
---
+++ assets/requirements.js
@@ -0,0 +1,44 @@
+/*
+  Group Project 3:
+
+  * Solid, semantic HTML
+  * Some CSS Framework (Bootstrap, Materialize, etc...)
+  * Back-end database (MySQL/Sequelize or MongoDB/Mongoose)
+  * React
+    * Props & state 
+    * Hooks (useState, useEffect)
+    * Context 
+    * React Router 
+    * Re-usable Components
+  * Back-end CRUD functionality via Express (RESTful API)
+  * Login/authentication capability 
+  * One new technology:
+    * Third-party React component 
+    * Plugins for Mongoose/Sequelize 
+    * Third-party API for data 
+    
+  * Solid code structure and organization (MVC pattern)
+  * Wise use of comments 
+  * Production-ready code
+  * Lots of commits, branching/merging etc 
+  * High-quality README file w/ link to deployed version
+  * Deployed to Heroku (and Mongo Atlas for MongoDB)
+  * Use of .env files for any sensitive info (API keys, passwords, secret phrases...)
+  
+  * Final presentation:
+    * Everyone speaks
+    * Slide show
+    * MVP 
+    * Challenges/Successes/Trouble spots 
+    * Technologies used
+    * Areas for expansion/improving the final product 
+    
+  * Grading teammates 
+
+  * Everyone particpates during development
+  * Be on time, on cam, etc. 
+  * Daily standups 
+  * Allow time for testing 
+
+
+*/
