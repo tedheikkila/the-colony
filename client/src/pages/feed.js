20936506203f2f5a0c723f8c26048d7d646d75df
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-19 20:23:56 -0500

    added sidebar to feed

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Form, Jumbotron } from 'react-bootstrap';
+import { Container, Button, Form, Jumbotron} from 'react-bootstrap';
 
 // import { getMe, deletePost } from '../utils/API';
 // import Auth from '../utils/auth';
@@ -8,12 +8,24 @@ import { Container, Button, Form, Jumbotron } from 'react-bootstrap';
 function feed() {
 
   return (
-    <Container className="feed-container">
-      <h2 className="the-feed">The Feed</h2>
-      <Form className="post-form">
-        <textarea className="post-text" placeholder="Type post here"></textarea>
-        <Button className="btn btn-primary" type="submit" id="submit-post">Send it</Button>
-      </Form>
+    <Container className="container-fluid">
+      <div className="row">
+        <div className="col-4 list-container">
+          <div className="card">
+            <ul className="list-group">
+              <Jumbotron className="feeds-sidebar">Feeds</Jumbotron>
+              <Button className="city-feed-btn">City Feed</Button>
+            </ul>
+          </div>
+        </div>
+        <Container className="feed-container col-8">
+          <h2 className="the-feed">The Feed</h2>
+          <Form className="post-form">
+            <textarea className="post-text" placeholder="Send message to Feed"></textarea>
+            <Button className="btn btn-primary" type="submit" id="submit-post">Send it</Button>
+          </Form>
+        </Container>
+      </div>
     </Container>
   );
 };
