3a0113600afd9a402e9ff106eb119ac177230040
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-19 19:31:44 -0500

    dev'ing feed

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { Container, Button, Form } from 'react-bootstrap';
 
 // import { getMe, deletePost } from '../utils/API';
 // import Auth from '../utils/auth';
 // import { removePostId } from '../utils/localStorage';
 
-function feed () {
+function feed() {
 
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <>
+      <Form className="post-form">
+        <textarea className="post-text" placeholder="Type post here"></textarea>
+        <Button className="btn btn-primary" type="submit" id="submit-post">Send it</Button>
+      </Form>
+    </>
   );
 };
 
