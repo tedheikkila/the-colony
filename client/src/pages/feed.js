a777533810c8826dfb06c54b6095be23edfdc133
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-19 19:44:28 -0500

    added the feed title

--- client/src/pages/feed.js
+++ client/src/pages/feed.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Form } from 'react-bootstrap';
+import { Container, Button, Form, Jumbotron } from 'react-bootstrap';
 
 // import { getMe, deletePost } from '../utils/API';
 // import Auth from '../utils/auth';
@@ -8,12 +8,13 @@ import { Container, Button, Form } from 'react-bootstrap';
 function feed() {
 
   return (
-    <>
+    <Container className="feed-container">
+      <h2 className="the-feed">The Feed</h2>
       <Form className="post-form">
         <textarea className="post-text" placeholder="Type post here"></textarea>
         <Button className="btn btn-primary" type="submit" id="submit-post">Send it</Button>
       </Form>
-    </>
+    </Container>
   );
 };
 
